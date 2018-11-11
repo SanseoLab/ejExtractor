ff0b4e600d2639f28f3eff2cc2f7c734bdacdf9d
SanseoLab <montouesto@gmail.com>
2018-11-11 13:56:41 +0900

    Update ejExtractor.py

--- ejExtractor.py
+++ ejExtractor.py
@@ -13,6 +13,7 @@ os.makedirs(res)
 
 def help():
 	print "USAGE : ejExtractor.py -[Option] [Path]"
+	print "USAGE2 (Powershell Secure String) : ejExtractor.py -[Option] [Path] -key [key]"
 	print "==== Option ===="
 	print "-h : Help"
 	print "-l : AutoHotKey version L"
@@ -22,6 +23,12 @@ def help():
 	print "-i : InnoSetup"
 	print "-n : NSIS"
 	print "-m : MSI"
+	print "-pdd : Powershell Deflate Decode"
+	print "-pde : Powershell Deflate Encode"
+	print "-pgd : Powershell GZip Decode"
+	print "-pge : Powershell GZip Encode"
+	print "-pse : Powershell Secure String Decode"
+	print "-psd : Powershell Secure String Encode"
 	return
 
   
@@ -34,138 +41,212 @@ def find_nth(s, x, n, i = 0):
   
  
 def main():
-	print res
-	try:
-		opts, args = getopt.getopt(sys.argv[1:],"lbaAinmh:o:",["input=","output=","help"])
-	except getopt.GetoptError as err:
-		print str(err)
-		sys.exit(1)
+
+	opt = sys.argv[1]
 	
 	if len(sys.argv) == 1:
 		help()
  
-	for opt,arg in opts:
-		if ( opt == "-l" ):
-		# autohotkey_L
-			f=sys.argv[2]
-			resFile = res + "\\result.txt"
-			f=open(f,'rb')
-			f=f.read()
-			n=f.find('\x3cCOMPILER:')
-			f=f[n:]
-			n=f.find('\x00\x00')
-			f=f[0:n]
-			f=f.replace('\x0a','\x0d\x0a')
-			q=open(resFile,'wb')
-			q.write(f)
-			q.close()
-		elif ( opt == "-b" ):
-		# autohotkey_B
-			os.chdir('AutoHK')
-			autohkDir = os.getcwd()
-			exe2ahkFile = autohkDir + "\\AutoHK_B.exe"
-			itsTemp = res + "\\AutoHK_B.exe"
-			itsTemp2 = res + "\\dumb.exe"
-			shutil.copy(exe2ahkFile, itsTemp)
-			shutil.copy(sys.argv[2], itsTemp2)
-			command = itsTemp + " " + itsTemp2
-			os.system(command)
-			os.remove(itsTemp)
-			os.remove(itsTemp2)
-		elif ( opt == "-a" ):
-		# AutoIt
-			os.chdir('AutoIt')
-			autoitDir = os.getcwd()
-			inputFile = sys.argv[2]
-			compiledscript = autoitDir + "\\compiledScript.bin"
-			dumbFile = autoitDir + "\\dumb.bin"
-			exe2autFile = autoitDir + "\\Exe2Aut.exe"
-			dumbbackFile = autoitDir + "\\dumb.bin.backup"
+
+	if ( opt == "-l" ):
+	# autohotkey_L
+		f=sys.argv[2]
+		resFile = res + "\\result.txt"
+		f=open(f,'rb')
+		f=f.read()
+		n=f.find('\x3cCOMPILER:')
+		f=f[n:]
+		n=f.find('\x00\x00')
+		f=f[0:n]
+		f=f.replace('\x0a','\x0d\x0a')
+		q=open(resFile,'wb')
+		q.write(f)
+		q.close()
+	elif ( opt == "-b" ):
+	# autohotkey_B
+		os.chdir('AutoHK')
+		autohkDir = os.getcwd()
+		exe2ahkFile = autohkDir + "\\AutoHK_B.exe"
+		itsTemp = res + "\\AutoHK_B.exe"
+		itsTemp2 = res + "\\dumb.exe"
+		shutil.copy(exe2ahkFile, itsTemp)
+		shutil.copy(sys.argv[2], itsTemp2)
+		command = itsTemp + " " + itsTemp2
+		os.system(command)
+		os.remove(itsTemp)
+		os.remove(itsTemp2)
+	elif ( opt == "-a" ):
+	# AutoIt
+		os.chdir('AutoIt')
+		autoitDir = os.getcwd()
+		inputFile = sys.argv[2]
+		compiledscript = autoitDir + "\\compiledScript.bin"
+		dumbFile = autoitDir + "\\dumb.bin"
+		exe2autFile = autoitDir + "\\Exe2Aut.exe"
+		dumbbackFile = autoitDir + "\\dumb.bin.backup"
 		
-			fr = open(inputFile, "rb")
-			# For read input file
-			fs = open(compiledscript, "wb")
-			# For output compiled script file
-			fd = open(dumbFile, "ab")
+		fr = open(inputFile, "rb")
+		# For read input file
+		fs = open(compiledscript, "wb")
+		# For output compiled script file
+		fd = open(dumbFile, "ab")
 
-			# Read input file and Extract Compiled script
-			readInput = fr.read()
+		# Read input file and Extract Compiled script
+		readInput = fr.read()
 
-			startAddr = find_nth(readInput, "AU3!EA06", 1) - 16
-			endAddr = find_nth(readInput, "AU3!EA06", 2) + 8
+		startAddr = find_nth(readInput, "AU3!EA06", 1) - 16
+		endAddr = find_nth(readInput, "AU3!EA06", 2) + 8
 
-			fs.write(readInput[startAddr:endAddr])
-			fs.close()
+		fs.write(readInput[startAddr:endAddr])
+		fs.close()
 
-			# Read compiled script file
-			fs = open(compiledscript, "rb")
-			r2 = fs.read()
+		# Read compiled script file
+		fs = open(compiledscript, "rb")
+		r2 = fs.read()
 
-			# Append compiled script file to dumb.bin
-			fd.write(r2)
+		# Append compiled script file to dumb.bin
+		fd.write(r2)
 
-			fr.close()
-			fs.close()
-			fd.close()
+		fr.close()
+		fs.close()
+		fd.close()
 		
-			itsTemp = res + "\\Exe2Aut.exe"
-			itsTemp2 = res + "\\dumb.bin"
-			shutil.copy(exe2autFile, itsTemp)
-			shutil.copy(dumbFile, itsTemp2)
+		itsTemp = res + "\\Exe2Aut.exe"
+		itsTemp2 = res + "\\dumb.bin"
+		shutil.copy(exe2autFile, itsTemp)
+		shutil.copy(dumbFile, itsTemp2)
 		
-			command = itsTemp + " -nogui -quiet " + itsTemp2
-			os.system(command)
+		command = itsTemp + " -nogui -quiet " + itsTemp2
+		os.system(command)
 		
-			os.remove(itsTemp)
-			os.remove(itsTemp2)
-			os.remove(compiledscript)
-			os.remove(dumbFile)
-			shutil.copy(dumbbackFile, dumbFile)
-		elif ( opt == "-A"):
-		# AutoIt Just
-			os.chdir('AutoIt' )
-			autoitDir = os.getcwd()
-			inputFile = sys.argv[2]
-			exe2autFile = autoitDir + "\\Exe2Aut.exe"
-			itsTemp = res + "\\Exe2Aut.exe"
-			itsTemp2 = res + "\\dumb.exe"
-			shutil.copy(exe2autFile, itsTemp)
-			shutil.copy(sys.argv[2], itsTemp2)
-			command = itsTemp + " -nogui -quiet " + itsTemp2
-			os.system(command)
-			os.remove(itsTemp)
-			os.remove(itsTemp2)
-		elif ( opt == "-i" ):
-		# InnoSetup
-			os.chdir('InnoSetup')
-			innoDir = os.getcwd()
-			innoFile = innoDir + "\\innounp47.exe"
-			command = innoFile + " -x " + sys.argv[2]
-			os.chdir(res)
-			os.system(command)
-		elif ( opt == "-n" ):
-		# NSIS
-			os.chdir('NSIS')
-			nsisDir = os.getcwd()
-			nsisFile = nsisDir + "\\7z.exe"
-			command = nsisFile + " e " + sys.argv[2]
-			os.chdir(res)
-			os.system(command)
-		elif ( opt == "-m" ):
-		# MSI
-			os.chdir('MSI')
-			msiDir = os.getcwd()
-			msiFile = msiDir + "\\jsMSIx.exe"
-			msiTemp = res + "\\temp.msi"
-			shutil.copy(sys.argv[2], msiTemp)
-			command = msiFile + " " + msiTemp
-			os.system(command)
-			os.remove(msiTemp)
-		elif ( opt == "-h"):
-			help()
+		os.remove(itsTemp)
+		os.remove(itsTemp2)
+		os.remove(compiledscript)
+		os.remove(dumbFile)
+		shutil.copy(dumbbackFile, dumbFile)
+	elif ( opt == "-A"):
+	# AutoIt Just
+		os.chdir('AutoIt' )
+		autoitDir = os.getcwd()
+		inputFile = sys.argv[2]
+		exe2autFile = autoitDir + "\\Exe2Aut.exe"
+		itsTemp = res + "\\Exe2Aut.exe"
+		itsTemp2 = res + "\\dumb.exe"
+		shutil.copy(exe2autFile, itsTemp)
+		shutil.copy(sys.argv[2], itsTemp2)
+		command = itsTemp + " -nogui -quiet " + itsTemp2
+		os.system(command)
+		os.remove(itsTemp)
+		os.remove(itsTemp2)
+	elif ( opt == "-i" ):
+	# InnoSetup
+		os.chdir('InnoSetup')
+		innoDir = os.getcwd()
+		innoFile = innoDir + "\\innounp47.exe"
+		command = innoFile + " -x " + sys.argv[2]
+		os.chdir(res)
+		os.system(command)
+	elif ( opt == "-n" ):
+	# NSIS
+		os.chdir('NSIS')
+		nsisDir = os.getcwd()
+		nsisFile = nsisDir + "\\7z.exe"
+		command = nsisFile + " e " + sys.argv[2]
+		os.chdir(res)
+		os.system(command)
+	elif ( opt == "-m" ):
+	# MSI
+		os.chdir('MSI')
+		msiDir = os.getcwd()
+		msiFile = msiDir + "\\jsMSIx.exe"
+		msiTemp = res + "\\temp.msi"
+		shutil.copy(sys.argv[2], msiTemp)
+		command = msiFile + " " + msiTemp
+		os.system(command)
+		os.remove(msiTemp)
+	elif ( opt == "-pdd" ):
+	# Powershell Deflate Decode
+		os.chdir('Powershell')
+		pshDir = os.getcwd()
+		pshFile = pshDir + '\\PshDecoder.ps1'
+		pshTemp = res + "\\temp.ps1"
+		shutil.copy(sys.argv[2], pshTemp)
+		command = "powershell.exe -executionpolicy bypass " + pshFile + " DeflateDecode " + pshTemp
+		os.chdir(res)
+		print resDir
+		os.system(command)
+		os.remove(pshTemp)
+	elif ( opt == "-pde" ):
+	# Powershell Deflate Encode
+		os.chdir('Powershell')
+		pshDir = os.getcwd()
+		pshFile = pshDir + '\\PshDecoder.ps1'
+		pshTemp = res + "\\temp.ps1"
+		shutil.copy(sys.argv[2], pshTemp)
+		command = "powershell.exe -executionpolicy bypass " + pshFile + " DeflateEncode " + pshTemp
+		os.chdir(res)
+		print resDir
+		os.system(command)
+		os.remove(pshTemp)
+	elif ( opt == "-pgd" ):
+	# Powershell Gzip Decode
+		os.chdir('Powershell')
+		pshDir = os.getcwd()
+		pshFile = pshDir + '\\PshDecoder.ps1'
+		pshTemp = res + "\\temp.ps1"
+		shutil.copy(sys.argv[2], pshTemp)
+		command = "powershell.exe -executionpolicy bypass " + pshFile + " GzipDecode " + pshTemp
+		os.chdir(res)
+		print resDir
+		os.system(command)
+		os.remove(pshTemp)
+	elif ( opt == "-pge" ):
+	# Powershell Gzip Encode
+		os.chdir('Powershell')
+		pshDir = os.getcwd()
+		pshFile = pshDir + '\\PshDecoder.ps1'
+		pshTemp = res + "\\temp.ps1"
+		shutil.copy(sys.argv[2], pshTemp)
+		command = "powershell.exe -executionpolicy bypass " + pshFile + " GzipEncode " + pshTemp
+		os.chdir(res)
+		print resDir
+		os.system(command)
+		os.remove(pshTemp)
+	elif ( opt == "-psd" ):
+	# Powershell Secure String Decode
+		if sys.argv[3] != '-key':
+			print 'error'
+			return
+		os.chdir('Powershell')
+		pshDir = os.getcwd()
+		pshFile = pshDir + '\\PshDecoder.ps1'
+		pshTemp = res + "\\temp.ps1"
+		shutil.copy(sys.argv[2], pshTemp)
+		command = "powershell.exe -executionpolicy bypass " + pshFile + " SecureDecode " + pshTemp + " " + sys.argv[4]
+		os.chdir(res)
+		print resDir
+		os.system(command)
+		os.remove(pshTemp)
+	elif ( opt == "-pse" ):
+	# Powershell Secure String Encode
+		if sys.argv[3] != '-key':
+			print 'error'
+			return
+		os.chdir('Powershell')
+		pshDir = os.getcwd()
+		pshFile = pshDir + '\\PshDecoder.ps1'
+		pshTemp = res + "\\temp.ps1"
+		shutil.copy(sys.argv[2], pshTemp)
+		command = "powershell.exe -executionpolicy bypass " + pshFile + " SecureEncode " + pshTemp + " " + sys.argv[4]
+		os.chdir(res)
+		print resDir
+		os.system(command)
+		os.remove(pshTemp)
+	elif ( opt == "-h"):
+		help()
  
- 
-		return
+
+	return
  
  
 if __name__ == '__main__':
